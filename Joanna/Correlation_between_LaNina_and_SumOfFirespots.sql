7ab0f8105ce4a2a90b28534e770958d76ccb4e4d
Joanna Zielińska <joanna.zielinska.1996.jz@gmail.com>
2023-02-15 21:33:43 +0100

    Added new files with correlation

---
+++ Joanna/Correlation_between_LaNina_and_SumOfFirespots.sql
@@ -0,0 +1,21 @@
+-- Correlation between La Nina and sum of firespots 
+
+with table_2 as 
+	(select year, phenomenon, severity,
+	sum(firespots) as sum_of_firespots,
+	case 
+	when severity = 'Weak' then 1
+	when severity = 'Moderate' then 2
+	when severity = 'Strong' then 3
+	when severity = 'Very Strong' then 4
+	end value_severity
+	from el_nino_la_nina enln 
+	join inpe_brazilian_amazon_fires ibaf
+	on enln."start year" =ibaf."year" 
+	where phenomenon='La Nina'
+	group by "year", phenomenon, severity
+	order by "year")
+
+	select
+	round(corr(sum_of_firespots, value_severity):: numeric, 2)
+	from table_2
