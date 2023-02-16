66bcf52766f40252e489c32e5a95380273ecb1c0
Joanna Zielińska <80065218+JoannaZielinska-96@users.noreply.github.com>
2023-02-16 21:21:11 +0100

    Delete Cumulative_distribution_for_the_dry_season.sql

--- Joanna/Cumulative_distribution_for_the_dry_season.sql
+++
@@ -1,7 +0,0 @@
--- Cumulative distribution for the dry season 
-
-select year, 
-sum_firespots_dryseason,
-cume_dist() over (order by sum_firespots_dryseason)
-from sum_of_firespots_dryseason 
-order by "year" 
